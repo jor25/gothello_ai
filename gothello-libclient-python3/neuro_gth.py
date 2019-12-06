d230620369747ad09cfa96072677f479da8a070a
Jordan Le <jor25@pdx.edu>
2019-12-06 11:39:24 -0800

    Added some comments to the functions.

--- gothello-libclient-python3/neuro_gth.py
+++ gothello-libclient-python3/neuro_gth.py
@@ -15,13 +15,20 @@ import model_arch as ma
 
 VIEW = False    # Switch this if we don't want to see game details
 
-
+# Bart function for piece generation
 def letter_range(letter):
     for i in range(5):
         yield chr(ord(letter) + i)
 
 
 def show_position():
+    """
+    Function that shows the position to the user through standard output.
+    Also creates the states to pass to models for prediction.
+    returns two flattened numpy arrays, one of size 50 being a one hot encode
+    and the other of size 25 being a flattened version of the output data.
+    Note: Built off of Bart's random.
+    """
     state = []  # Making a new state
     scoring_state = [] # Custom for counting
     for digit in letter_range('1'):
@@ -54,6 +61,14 @@ def show_position():
 
 
 def make_predict(restate):
+    """
+    Function given the 50 element state of 1's and 0's
+    uses a neuronetwork prediction to determine a move.
+    Then error checks to see if that move is valid to play
+    based on the remaining contents of board.
+    Later: do a search based on the top 3 predictions (if valid)
+    return: string - my_move 
+    """
 
     moves = ['a5','b5','c5','d5','e5',
             'a4','b4','c4','d4','e4',
@@ -70,23 +85,31 @@ def make_predict(restate):
     #prediction[0][index] = -1
 
     if my_move in list(board):
+        #print(board)
         #print("Valid")
         return my_move  # this move is valid so do it
     else:
-        
+        count = 1
         while my_move not in list(board):   # While my move is not valid
             #print("{} is not valid".format(my_move))
             # Get a new prediction:
             index = np.argmax(prediction[0])    
-            prediction[0][index] = -1               # Previous prediction not good, try next best valid.
+            prediction[0][index] = -10               # Previous prediction not good, try next best valid.
             my_move = moves[np.argmax(prediction[0])]   # Get a move that is valid?
+            count += 1
         
-
+        if VIEW:
+            print("op-{}: {}".format(count, my_move))
         return my_move  # Next option
         #return random.choice(list(board))   # Legit, do something better than this later
 
 
 def calc_score(npstate):
+    """
+    Figure out who won this game. Winner has the most pieces on the board
+    at the end of the game.
+    return: 1 to add a point for winning, 0 if I lost or draw
+    """
     my_tiles = np.where(npstate == 2)[0]    # Where I(BLACK) placed my tiles
     opp_tiles = np.where(npstate == 1)[0]   # Where white placed their tiles
     if VIEW:
@@ -108,7 +131,12 @@ def calc_score(npstate):
         print("DRAW")
         return 0
 
+# Building off Bart's code
 def play_game(side):
+    """
+    Play the game and make moves against an opponent
+    based on states collected.
+    """
     while True:
         npstate, score_state = show_position()
         restate = np.reshape(npstate, (-1,50))
@@ -167,7 +195,9 @@ board = {letter + digit
 
 grid = {"white": set(), "black": set()}
 '''
-
+# Where the good stuff happens
+# Loop through and play x amount of games to determine how good
+# the ai approach is.
 if __name__ == "__main__":
 
     wins = 0
@@ -198,51 +228,4 @@ if __name__ == "__main__":
             print("ERROR on {}".format(i))
             i = i-1
             pass
-
-
-    #print(board)
-    '''
-    while True:
-        npstate = show_position()
-        restate = np.reshape(npstate, (-1,25))
-        print()
-        if side == me:
-            #move = random.choice(list(board))
-            #prediction = nn.model.predict(restate)
-            move = make_predict(restate)
-            print("me:", move)
-            try:
-                client.make_move(move)
-                grid[me].add(move)
-                board.remove(move)
-            except gthclient.MoveError as e:
-                if e.cause == e.ILLEGAL:
-                    print("me: made illegal move, passing")
-                    client.make_move("pass")
-        else:
-            cont, move = client.get_move()
-            print("opp:", move)
-            if cont and move == "pass":
-                print("me: pass to end game")
-                client.make_move("pass")
-                break
-            else:
-                if not cont:
-                    break
-                board.remove(move)
-                grid[opp].add(move)
-
-        side = gthclient.opponent(side)
-
-    # Get the score for winner here:
-    calc_score(npstate)
-    '''
-
-'''
-if __name__ == "__main__":
-    nn = Collection()
-    nn.model.predict(state)
-'''
-
-
 
